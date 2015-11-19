b2b139f26e087ce60ac810b907f28f583cd0f711
Jon Thacker <thacker.jon@gmail.com>
2015-11-19 15:04:19 -0600

    Modified traffic light example to show state more clearly

--- examples/traffic_light/main.c
+++ examples/traffic_light/main.c
@@ -6,15 +6,17 @@
 
 int main(int argc, char** argv) {
     smg_traffic_light_state_machine_t sm;
-    uint32_t cars;
+    uint32_t cars, last_cars;
 
     srand(0);
     smg_traffic_light_init(&sm);
     cars = 0;
+    last_cars = 0;
 
 
     while (true) {
         if (rand() % 10 == 0) {
+            printf("car has arrived\n");
             cars += 1;
         }
 
@@ -25,19 +27,20 @@ int main(int argc, char** argv) {
         switch (sm.state) {
             case SMG_TRAFFIC_LIGHT_STATE_GREEN:
             case SMG_TRAFFIC_LIGHT_STATE_YELLOW:
-                printf("car has left, %u remain\n", cars);
                 if (cars > 0) {
                     cars -= 1;
+                    printf("car has left, %u remain\n", cars);
                 }
                 break;
             case SMG_TRAFFIC_LIGHT_STATE_RED:
-                if (cars > 0) {
+                if (last_cars != cars) {
                     printf("%u cars are waiting\n", cars);
                 }
                 break;
             default:
                 break;
         }
+        last_cars = cars;
         usleep(50000);
     }
 
